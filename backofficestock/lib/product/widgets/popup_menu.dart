e059cd63d27e6141dbc736c09293fb5231456430
Özberk Şen <96706849+Ozberksenn@users.noreply.github.com>
2025-02-01 02:28:13 +0300

    post menu

---
+++ backofficestock/lib/product/widgets/popup_menu.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+PopupMenuButton<dynamic> popupMenu({
+  Widget? widget,
+  List<PopupMenuEntry<dynamic>>? items,
+}) {
+  return PopupMenuButton(
+    position: PopupMenuPosition.under,
+    child: widget,
+    itemBuilder: (context) => (items ?? [])
+        .map((e) => PopupMenuItem(child: e)) // e'yi doğrudan kullan
+        .toList(),
+  );
+}
--- backofficestock/lib/view/home/widgets/content_header.dart
+++ backofficestock/lib/view/home/widgets/content_header.dart
@@ -1,7 +1,10 @@
 import 'package:backofficestock/core/widget/padding.dart';
+import 'package:backofficestock/product/widgets/custom_dialog.dart';
+import 'package:backofficestock/view/form/form_view.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import '../../../product/widgets/custom_icon_button.dart';
+import '../../../product/widgets/popup_menu.dart';
 import '../../../product/widgets/search_field.dart';
 
 class ContentHeader extends StatelessWidget {
@@ -17,13 +20,26 @@ class ContentHeader extends StatelessWidget {
           title,
           style: Theme.of(context).textTheme.headlineMedium,
         ),
-        const Row(children: [
-          CustomIconButton(
-            icon: CupertinoIcons.add,
-            text: 'Add',
-          ),
-          CustomSizedBox.paddingWidth(widthValue: 8.0),
-          SearchField()
+        Row(children: [
+          popupMenu(
+              widget: const CustomIconButton(
+                icon: CupertinoIcons.add,
+                text: 'Add',
+              ),
+              items: [
+                PopupMenuItem(
+                    onTap: () => customDialog(context,
+                        widget: FormView(
+                            dialogContext: context,
+                            apiUrl: "/postMenu",
+                            edit: false,
+                            title: "Add Menu",
+                            route: "menu")),
+                    child: const Text('Add Menu')),
+                PopupMenuItem(onTap: () {}, child: const Text('Add Product'))
+              ]),
+          const CustomSizedBox.paddingWidth(widthValue: 8.0),
+          const SearchField()
         ])
       ],
     );

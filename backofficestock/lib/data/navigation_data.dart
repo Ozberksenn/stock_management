b1468002350a7f6f1ac8d8be815f0eee30eae8af
Özberk Şen <96706849+Ozberksenn@users.noreply.github.com>
2025-02-23 20:04:33 +0300

    fixed menus

--- backofficestock/lib/data/navigation_data.dart
+++ backofficestock/lib/data/navigation_data.dart
@@ -2,10 +2,10 @@ import 'package:iconsax/iconsax.dart';
 import '../product/model/navigation_model.dart';
 
 List<NavigationModel> navigationData = [
-  NavigationModel(id: 1, title: "Stok yönetimi", icon: Iconsax.element_4),
+  NavigationModel(id: 1, title: "Stock Management", icon: Iconsax.element_4),
   NavigationModel(id: 2, title: "QR", icon: Iconsax.scan),
-  NavigationModel(id: 3, title: "Satış", icon: Iconsax.dollar_circle),
-  NavigationModel(id: 4, title: "Raporlar", icon: Iconsax.document),
-  NavigationModel(id: 5, title: "Web Site", icon: Iconsax.designtools),
-  NavigationModel(id: 6, title: "Bildirimler", icon: Iconsax.notification),
+  NavigationModel(id: 3, title: "Sales", icon: Iconsax.dollar_circle),
+  NavigationModel(id: 4, title: "Web", icon: Iconsax.designtools),
+  NavigationModel(id: 5, title: "Notifications", icon: Iconsax.notification),
+  NavigationModel(id: 6, title: "Company", icon: Iconsax.setting_2),
 ];

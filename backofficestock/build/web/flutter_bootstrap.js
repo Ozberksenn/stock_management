914ac48d4fdc1aef9eb71a3ed01410e32eb85889
Özberk Şen <96706849+Ozberksenn@users.noreply.github.com>
2025-03-14 01:25:10 +0300

    new build

--- backofficestock/build/web/flutter_bootstrap.js
+++ backofficestock/build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3519206410"
+    serviceWorkerVersion: "2408371711"
   }
 });

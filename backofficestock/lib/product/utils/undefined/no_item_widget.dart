3ad6368bc226a23647bfb364e865835c44a2463b
Özberk Şen <96706849+Ozberksenn@users.noreply.github.com>
2025-05-20 00:16:54 +0300

    updated

--- backofficestock/lib/product/utils/undefined/no_item_widget.dart
+++ backofficestock/lib/product/utils/undefined/no_item_widget.dart
@@ -1,15 +1,29 @@
-import 'package:backofficestock/core/extension/context_extension.dart';
+// import 'package:backofficestock/core/extension/context_extension.dart';
+// import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:lottie/lottie.dart';
+// import 'package:iconsax/iconsax.dart';
+// import 'package:lottie/lottie.dart';
 
 class NoItemWidget extends StatelessWidget {
-  const NoItemWidget({super.key});
+  final String? message;
+  const NoItemWidget({super.key, this.message});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Lottie.asset('json/nothing.json',
-          width: context.dynamicWidth(0.2), height: context.dynamicHeight(0.2)),
-    );
+    return SizedBox(
+        child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        const Icon(
+          Icons.emoji_food_beverage_rounded,
+          size: 100,
+        ),
+        Text(message ?? "")
+      ],
+    )
+        //  Lottie.asset('json/nothing.json',
+        //     width: context.dynamicWidth(0.2), height: context.dynamicHeight(0.2)),
+        );
   }
 }

0500b3ff1b31b652b0b36a66dcfb16f9a622e718
Özberk Şen <96706849+Ozberksenn@users.noreply.github.com>
2025-06-06 01:48:19 +0300

    updated

--- backofficestock/lib/view/check/model/table_model.dart
+++ backofficestock/lib/view/check/model/table_model.dart
@@ -3,18 +3,14 @@ class TableModel {
   final String tableNo;
   final String status;
   final String? customerName;
-  final int? customerCount;
   final String? specialText;
-  final int companyId;
 
   TableModel({
     required this.id,
     required this.tableNo,
     required this.status,
     this.customerName,
-    this.customerCount,
     this.specialText,
-    required this.companyId,
   });
 
   factory TableModel.fromMap(Map<String, dynamic> json) {
@@ -23,9 +19,7 @@ class TableModel {
       tableNo: json['TABLE_NO'],
       status: json['STATUS'],
       customerName: json['CUSTOMER_NAME'],
-      customerCount: json['CUSTOMER_COUNT'],
       specialText: json['SPECIAL_TEXT'],
-      companyId: json['COMPANY_ID'],
     );
   }
 

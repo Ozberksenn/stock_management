e25c3c3097fa54df475ca2dc990d64db5e3e9c47
Özberk Şen <96706849+Ozberksenn@users.noreply.github.com>
2025-06-08 17:54:20 +0300

    updated

--- backofficestock/lib/view/check/table_form.dart
+++ backofficestock/lib/view/check/table_form.dart
@@ -9,26 +9,47 @@ class TableForm extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          FormLabel(text: "Table No"),
-          FormTextField(name: "TABLE_NO"),
-          CustomSizedBox.paddingHeight(heightValue: 12.0),
-          FormLabel(text: "Status"),
-          FormDropdownField(
-            fieldName: "STATUS",
-            items: ["Active", "Reserved", "Deactive"],
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const CustomExpanded(
+          child:
+              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+            FormLabel(text: "Table No"),
+            FormTextField(name: "TABLE_NO"),
+            CustomSizedBox.paddingHeight(heightValue: 12.0),
+            FormLabel(text: "Status"),
+            FormDropdownField(
+              fieldName: "STATUS",
+              items: ["Active", "Reserved", "Deactive"],
+            ),
+            CustomSizedBox.paddingHeight(heightValue: 12.0),
+            FormLabel(text: "Customer Name"),
+            FormTextField(name: "CUSTOMER_NAME"),
+            CustomSizedBox.paddingHeight(heightValue: 12.0),
+            FormLabel(text: "Customer Count"),
+            FormTextField(name: "CUSTOMER_COUNT"),
+            CustomSizedBox.paddingHeight(heightValue: 12.0),
+            FormLabel(text: "Special Text"),
+            FormTextField(name: "SPECIAL_TEXT"),
+          ]),
+        ),
+        const CustomSizedBox.paddingWidth(widthValue: 12.0),
+        CustomExpanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const FormLabel(text: "Products"),
+              ListView.builder(
+                  itemCount: 4,
+                  shrinkWrap: true,
+                  itemBuilder: (context, index) {
+                    return Text("Product $index");
+                  })
+            ],
           ),
-          CustomSizedBox.paddingHeight(heightValue: 12.0),
-          FormLabel(text: "Customer Name"),
-          FormTextField(name: "CUSTOMER_NAME"),
-          CustomSizedBox.paddingHeight(heightValue: 12.0),
-          FormLabel(text: "Customer Count"),
-          FormTextField(name: "CUSTOMER_COUNT"),
-          CustomSizedBox.paddingHeight(heightValue: 12.0),
-          FormLabel(text: "Special Text"),
-          FormTextField(name: "SPECIAL_TEXT"),
-        ]);
+        )
+      ],
+    );
   }
 }

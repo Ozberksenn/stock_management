dc389612b77f2ad760c0c4f4f51194b09f3e728a
Özberk Şen <96706849+Ozberksenn@users.noreply.github.com>
2025-07-30 23:30:40 +0300

    updated

--- backofficestock/build/web/flutter_service_worker.js
+++ backofficestock/build/web/flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "03f2de9cb09cb2ce5cefb9f7478a6a91",
+const RESOURCES = {"flutter_bootstrap.js": "816c42b5f29fe7934b7eaf33377454bd",
 "version.json": "0b5abdf91a7b6f13d8974d8cc3d186c4",
 "index.html": "6e593d316f19baf4a0142ecd46ad1898",
 "/": "6e593d316f19baf4a0142ecd46ad1898",
-"main.dart.js": "d2126b0bf9dfb0261d685dc47a2d7e0b",
+"main.dart.js": "fb1c6622556dd748ecbd826486a8d4a5",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
 "favicon.png": "ea9d6b4b1f9854a3c5a70c93357dd5bd",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
